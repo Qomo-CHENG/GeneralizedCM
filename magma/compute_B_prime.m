f09ffa43db7f86eb4f23d6a29e6722a2b218f1c2
Qomo-CHENG <wei.cheng.cn18@gmail.com>
2020-11-23 01:08:43 +0100

    Exhibiting some codes with different dual distance d and B'_d.

--- magma/compute_B_prime.m
+++ magma/compute_B_prime.m
@@ -4,16 +4,22 @@
 @ Wei Cheng, 17-08-2020
 **/
 
-GF256<alpha> := GF(2^8);
+GF_K<alpha> := GF(2^8);
 
-C := LinearCode<GF256, 3|[1, 1, 1]>;
-D := LinearCode<GF256, 3|[alpha, alpha^73, alpha^81]>;
-
+j := 72;
+k := 80;
+C := LinearCode<GF_K, 3|[1, 1, 1]>;
+D := LinearCode<GF_K, 3|[1, alpha^j, alpha^k]>;
+						  
 C_dual_b := SubfieldRepresentationCode(Dual(C), GF(2));
 D_dual_b := SubfieldRepresentationCode(Dual(D), GF(2));
 d_dual := MinimumWeight(D_dual_b);
 
-CWs := [[x, x+a]: x in D_dual_b, a in (D_dual_b meet C_dual_b) | (x+a in D_dual_b) and (Weight(x) eq d_dual) and (Weight(x+a) eq d_dual)];
+X_s := [x: x in D_dual_b | (x notin C_dual_b) and (Weight(x) eq d_dual)];
+B_d_p := [[x,y]: x in X_s, y in X_s | x+y in C_dual_b];
 
-print "B'_d: ", #CWs; // Number of codewords satisfy all conditions
-print "Codewords: ", CWs; // Output all combinations of (x, y)
+print "D_dual_b: ", D_dual_b;
+print "Weight distribution: ", WeightDistribution(D_dual_b);
+print "d_dual: ", d_dual; // dual distance of D
+print "B'_d: ", #B_d_p; // Number of codewords satisfy all conditions
+print "Codewords: ", B_d_p; // Output all combinations of (x, y)
